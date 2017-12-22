7b616b645f49e879e49431235c975c72720649fd
yanjingyang@zulong.com <yanjingyang@zulong.com>
2017-12-22 19:12:59 +0800

    注册中心

--- src/test/java/com/yjy/provider/Provider.java
+++ src/test/java/com/yjy/provider/Provider.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Provider {
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:dubbo-provider.xml"});
         context.start();
         System.out.println("服务开始运行...");
         System.in.read(); // 按任意键退出
